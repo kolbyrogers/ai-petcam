b873071ad5403b29d7d1101f9bc3451fcc4c1ddf
Kolby Rogers <kolbyrogers1@gmail.com>
2023-02-08 15:02:52 -0700

    fix event images

--- components/Event.vue
+++ components/Event.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="mx-auto my-6">
-    <v-img src="event.imgUrl" height="200px"></v-img>
+    <v-img :src="event.imgUrl" height="300px"></v-img>
     <v-card-title> {{ event.name }} at the {{ event.object }} </v-card-title>
     <v-card-subtitle>{{ event.time }}</v-card-subtitle>
   </v-card>
