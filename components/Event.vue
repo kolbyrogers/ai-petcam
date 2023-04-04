d23fcb6197a6a300cd48783d96d0b990f01e8bc6
Kolby Rogers <kolbyrogers1@gmail.com>
2023-04-04 16:19:23 -0600

    add delete button to events

--- components/Event.vue
+++ components/Event.vue
@@ -1,8 +1,9 @@
 <template>
-  <v-card class="mx-auto my-6">
+  <v-card v-if="!deleted" class="mx-auto my-6">
     <v-img :src="event.imgUrl" height="300px"></v-img>
     <v-card-title> {{ event.name }} near {{ event.object }} </v-card-title>
     <v-card-subtitle>{{ event.time }}</v-card-subtitle>
+    <v-btn class="ma-3" @click="deleteEvent(event._id)">Delete</v-btn>
   </v-card>
 </template>
 
@@ -11,7 +12,22 @@ export default {
   props: ['event'],
   data: () => ({
     show: false,
+    deleted: false,
   }),
+  methods: {
+    async deleteEvent(eventID) {
+      let confirm = window.confirm('Are you sure you want to delete this event?')
+      if (!confirm) return
+      try {
+        let res = await this.$axios.delete(`http://localhost:3000/api/events/${eventID}`)
+        if (res.status === 200) {
+          this.deleted = true
+        }
+      } catch(err) {
+        console.error(err)
+      }
+    },
+  },
 }
 </script>
 <style>
