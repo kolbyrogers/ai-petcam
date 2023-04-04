cf22448a0644924376bffb26bc3bb7fdf45ddd91
Kolby Rogers <kolbyrogers1@gmail.com>
2023-04-03 21:26:13 -0600

    put mongodb uri in .env

--- api/model.js
+++ api/model.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 
-mongoose.connect(
-  'mongodb+srv://krogers:MwgGeaBR8Fpswklj@cluster0.ka7qd.mongodb.net/petcam?retryWrites=true&w=majority'
-)
+mongoose.connect(process.env.MONGODB)
 
 const Event = mongoose.model('Event', {
   userid: {
